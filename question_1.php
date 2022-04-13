a0be89e2099214492c2b9f232e79ea8584a249b5
Joji <freedive.cebu30@gmail.com>
2022-04-13 16:39:57 +0800

    Create question_1.php

---
+++ question_1.php
@@ -0,0 +1,18 @@
+<?php
+
+$number_1 = 10;
+$number_2 = 3;
+$number_3 = 2;
+$number_4 = 10000;
+
+printf("%s + %s = %s", $number_1, $number_2, $number_1 + $number_2);
+echo PHP_EOL;
+printf("%s - %s = %s", $number_1, $number_2, $number_1 - $number_2);
+echo PHP_EOL;
+printf("%s * %s = %s", $number_1, $number_2, $number_1 * $number_2);
+echo PHP_EOL;
+printf("%s / %s = %s", $number_1, $number_2, $number_4 / $number_1);
+echo PHP_EOL;
+printf("%sの2乗 = %s", $number_3, $number_3 ** 2);
+echo PHP_EOL;
+printf("%sの3乗 = %s", $number_3, $number_3 ** 3);
