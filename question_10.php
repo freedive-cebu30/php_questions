022e82060aa2469d1027bc85bbbe2100db2341ed
Joji <freedive.cebu30@gmail.com>
2022-04-14 15:56:30 +0800

    Create question_10.php

---
+++ question_10.php
@@ -0,0 +1,33 @@
+<?php
+
+
+# どちらのメソッドでも大丈夫です
+# Either method is fine
+
+function cal_cast($number) {
+  $s_number = (string)$number;
+  $three_times_number = str_repeat($s_number, 3);
+  $i_number = (int)$three_times_number;
+
+  return $number + $i_number;
+}
+
+function cal($number) {
+  $three_times_number = str_repeat($number, 3);
+
+  return $number + $three_times_number;
+}
+
+print(cal_cast(3));
+echo PHP_EOL;
+print(cal_cast(4));
+echo PHP_EOL;
+print(cal_cast(10));
+echo PHP_EOL;
+
+print(cal(3));
+echo PHP_EOL;
+print(cal(4));
+echo PHP_EOL;
+print(cal(10));
+echo PHP_EOL;
