5e818bc75fee395b30c56404c67cd3e4a4388a75
Joji <freedive.cebu30@gmail.com>
2022-04-15 23:05:15 +0800

    Update question_19.php

--- question_19.php
+++ question_19.php
@@ -1,6 +1,8 @@
 <?php
 
 
+// どちらの関数でも大丈夫です
+// either function is fine
 function str_length($v1, $v2)
 {
     if ($v1 == $v2) {
@@ -9,7 +11,13 @@ function str_length($v1, $v2)
     return (strlen($v1) < strlen($v2)) ? -1 : 1;
 }
 
+function str_length_2($v1, $v2)
+{
+    return strlen($v1) <=> strlen($v2);
+}
+
 $lang = array("javaScript", "ruby", "php", "python");
 usort($lang, 'str_length');
+usort($lang, 'str_length_2');
 
 print_r($lang);
