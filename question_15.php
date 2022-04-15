e40618011d9f26e92167371fc3e12eb4eb2939ed
Joji <freedive.cebu30@gmail.com>
2022-04-15 15:29:16 +0800

    Create question_15.php

---
+++ question_15.php
@@ -0,0 +1,17 @@
+# array_sumを使った方法
+# the way of array_sum
+$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+$res = array_sum($arr);
+
+print($res);
+echo PHP_EOL;
+
+
+# eachを使った方法
+# the way of foreach
+$sum = 0;
+foreach ($arr as $val) {
+  $sum += $val;
+}
+
+print($sum);
