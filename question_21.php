dbcfbbcf18dd7642534ef1f81ab024220fbe9fb7
Joji <freedive.cebu30@gmail.com>
2022-04-16 22:24:48 +0800

    Update question_21.php

--- question_21.php
+++ question_21.php
@@ -4,13 +4,13 @@
 $arr1 = [1, 2, 3, 4, 5, 8, 9, 10, 18, 20];
 $arr2 = [1, 2, 3, 4, 5, 8, 10, 20];
 
-var_dump(multiple_9($arr1));
-var_dump(multiple_9($arr2));
+var_dump(multiple($arr1, 9));
+var_dump(multiple($arr2, 9));
 
-function multiple_9($arr) {
+function multiple($arr, $num) {
   $res = false;
   foreach ($arr as $val) {
-    if ($val % 9 == 0) {
+    if ($val % $num == 0) {
       return true;
     }
   }
