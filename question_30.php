591c1f82f954c99c82c70ad2a1ba52853368d7ca
Joji <freedive.cebu30@gmail.com>
2022-04-17 14:54:06 +0800

    Create question_30.php

---
+++ question_30.php
@@ -0,0 +1,11 @@
+<?php
+// Your code here!
+
+$v1 = [1, 2, 3, 4, 5, 8, 9];
+$v2 = [9, 7, 6, 5, 4];
+
+$result = array_diff($v1, $v2);
+$result2 = array_diff($v2, $v1);
+
+$answer = array_merge($result, $result2);
+print_r($answer);
