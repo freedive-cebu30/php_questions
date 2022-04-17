4049f5e639ebfc9a782c507e4b738cced2e87f06
Joji <freedive.cebu30@gmail.com>
2022-04-17 13:07:30 +0800

    Create question_25.php

---
+++ question_25.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$arr = ["Ms. Tanaka", "Mr. Suzuki", "Ms. Akagi", "Mrs. Yoko"];
+
+
+$res = [];
+foreach ($arr as $val) {
+  if(str_starts_with($val, 'Ms.')){
+    array_push($res, $val); 
+  }
+}
+
+print_r($res);
+
+
+$res = [];
+foreach ($arr as $val) {
+  if(str_ends_with($val, 'i')){
+    array_push($res, $val); 
+  }
+}
+
+print_r($res);
