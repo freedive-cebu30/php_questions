a46a0bc8f81485313827c2d77addb179b999dc6c
Joji <freedive.cebu30@gmail.com>
2022-04-18 11:30:55 +0800

    Update question_34.php

--- question_34.php
+++ question_34.php
@@ -7,4 +7,16 @@ $arr = [array('name' => 'Taro_1', 'age' => 20, 'height' => 180, 'weight' => 60, 
         array('name' => 'Taro_4', 'age' =>  50, 'height' => 150, 'weight' => 75, 'country' => 'Japan' )];
 
 
+// array_filterを使うと、簡単に書けます
+// You can write a code simply using array_filter
 print_r(array_filter($arr, fn($v) => array_key_exists('country', $v)));
+
+
+$res = array();
+foreach ($arr as $v) {
+  if(array_key_exists('country', $v)){
+    array_push($res, $v);
+  }
+}
+
+print_r($res);
