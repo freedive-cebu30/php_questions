6ab2b4a20698aff34ded2b4e9e57ca712a570625
Joji <freedive.cebu30@gmail.com>
2022-04-18 16:54:58 +0800

    Create question_46.php

---
+++ question_46.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$value = ['a', 'b', 'c', 'd'];
+
+[$v1, $v2, $v3, $v4] = $value;
+
+print($v1);
+echo PHP_EOL;
+print($v2);
+echo PHP_EOL;
+print($v3);
+echo PHP_EOL;
+print($v4);
+echo PHP_EOL;
