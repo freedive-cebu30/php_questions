4743ec43be7ba6501fdf1d0bc83f8ee03ea31433
Joji <freedive.cebu30@gmail.com>
2022-04-18 22:40:49 +0800

    Update question_31.php

--- question_31.php
+++ question_31.php
@@ -4,6 +4,9 @@
 $str = '99,100,201,101,9999,2,5,6';
 $arr = explode(",", $str);
 $res = array_filter($arr, 'minimum_100');
+# このように書くこともできます
+# You can also write a code like this.
+# $res = array_filter($arr, fn($v) => $v > 100);
 rsort($res);
 $answer = join(" ", $res);
 print($answer);
