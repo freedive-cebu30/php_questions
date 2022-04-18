6d9852af657147e3fd9f5f67a36e46d34c6f4cb7
Joji <freedive.cebu30@gmail.com>
2022-04-18 12:10:11 +0800

    Update question_34.php

--- question_34.php
+++ question_34.php
@@ -4,7 +4,7 @@
 $arr = [array('name' => 'Taro_1', 'age' => 20, 'height' => 180, 'weight' => 60, 'country' => 'Japan'),
         array('name' => 'Taro_2', 'age' => 30, 'height' => 170, 'weight' => 65 ),
         array('name' => 'Taro_3', 'age' => 40, 'height' => 160, 'weight' => 70, 'country' => 'Taiwan'),
-        array('name' => 'Taro_4', 'age' =>  50, 'height' => 150, 'weight' => 75, 'country' => 'Japan' )];
+        array('name' => 'Taro_4', 'age' => 50, 'height' => 150, 'weight' => 75, 'country' => 'Japan' )];
 
 
 // array_filterを使うと、簡単に書けます
